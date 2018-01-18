98cdba049b1c2447c721c9f0215cd1c12fd8697c
Bradley Schoebinger <Bradleysoulpet@gmail.com>
2018-01-18 11:15:05 -0700

    worked on my animal class (added chicken art from the asset strore)

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	// This declhairs what start will do
+	void Start () {
+		print("My animal is eating.");
+		print("Now my animal is sleeping.");
+		print("Hahaha now he is dead becuase he fell off of a cliff");
+	}
+
+	void OnMouseDown()
+	{
+		print("I'm dead");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
