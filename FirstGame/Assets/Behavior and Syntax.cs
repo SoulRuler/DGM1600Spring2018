70e5166f8bb06e07a1aa1117ca174114e3afc584
Bradley Schoebinger <Bradleysoulpet@gmail.com>
2018-01-23 11:07:16 -0700

    I talked about boxes, how you need brackets and how you can do anything

---
+++ FirstGame/Assets/Behavior and Syntax.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorandSyntax : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+//So everything is in boxes which take comands and follow orders
+
+//Pretty much everything is possible when you are coding
+
+//Start starts, print prints, it is very simple when it comes down to everything
+
+//everything has to be in brackets for it to work  (the boxes are like space helmets, you cannot live with out them on)
+
+//void isnt nothing it is just saying it does work and does not give anything back. 
+
+//you can have several boxes doing similar things or several boxes with in themselves
