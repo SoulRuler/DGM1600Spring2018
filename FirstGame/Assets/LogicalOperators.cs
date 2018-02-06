b9e4a52c99b041e82cece6b053c71c2739a175b2
Bradley Schoebinger <Bradleysoulpet@gmail.com>
2018-02-06 11:10:00 -0700

    We learned about powerups and logical code

---
+++ FirstGame/Assets/LogicalOperators.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalOperators : MonoBehaviour {
+
+public int a;
+public int b;
+public string password;
+
+	void start()
+	{
+
+print("are you ready for this");
+print("bum dum bum bum bum bum dum bum bum bum bum dum dum bum bu mbu mbum bu bmdubmbum");
+
+	}
+
+	void OnMouseDown()
+	{
+		
+		if(a < b)
+		{
+			print("a is less then b");
+		}
+
+		if(a == b)
+		{
+			print("a is equal to b");
+		}
+
+		if(a > b)
+		{
+			print("a is greater then b");
+		}
+		
+		if(a != b && password != "wootwoot")
+		{
+			print("you dun messed up");
+		}
+
+		if(a == b && password == "wootwoot")
+		{
+			print("wootwoot you did it");
+		}
+	}
+
+}
+
+
+//We started off by talking about different opperators
+
+//&& means also
+
+//|| means or
+
+//we talked about if a == b or a < b so on so forth
+
+//we partied becuase we were bored
+
+//a lot of if statments with passwords and logic stuff 
+
+//there are more operators to talk about
+
+//we were introduced to a scriptable object "power up"
+
+//the anology was mad about chettahs and elephants
