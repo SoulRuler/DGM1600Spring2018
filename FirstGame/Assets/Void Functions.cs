665f56cd576e198e4ff0542d386bd1ce333130eb
Bradley Schoebinger <Bradleysoulpet@gmail.com>
2018-02-27 14:14:11 -0700

    Void Funcations and makeing your own

---
+++ FirstGame/Assets/Void Functions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoidFunctions : MonoBehaviour {
+
+//void run; 
+
+
+	// Use this for initialization
+	//void Start () {
+		
+	//	run(print " I have made a void function");
+
+	//}
+
+}
+
+
+//Funcations allow you to make a function with instructions
+
+//They also allow you to re use your instructions as long as you want
+
+//You are allowed to make your own voids
+
+//as in void run so you dont have to write it out every single time
+
+//You can make anything a voide when it comes to the functions
+
+//As seen above i have made fun a voide and put it in the start void function
