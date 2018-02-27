6777858d4020703b92946eb3ae40ad1f55749bce
Bradley Schoebinger <Bradleysoulpet@gmail.com>
2018-02-27 10:21:00 -0700

    All the loops with notes

---
+++ FirstGame/Assets/AllTheLoops.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllTheLoops : MonoBehaviour {
+
+	int CupsInTheSink = 4;
+
+	int NumEnemies = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	While(CupsInTheSink > 0){
+			Debug.Log ("I've washed a cup!");
+			CupsInTheSink--;
+	}
+
+	bool ShouldContinue = flase;
+
+	do
+	{
+
+		print ("Hello world");f
+
+	}while(ShouldContinue == true);
+
+	for(int i = 0; i < NumEnemies; i++){
+
+		Debug.Log("Creating enemy number: " + i)
+
+	}
+
+	string[] strings = new strings[3];
+
+	strings[0] = "first string";
+	strings[1] = "second strings";
+	strings[2] = "Thirds string";
+
+	foreach (string item in strings)
+	{
+		print (item);
+	}
+
+	}
+}
+
+//Looping is also interpreted as interating
+
+//loops repeat
+
+//Start with while and it will continue to repeat if the conditions are true
+
+//this code will keep minusing cups until it gets to zero
+
+//do while do it at the end while while does it at the beginnign. 
+
+//we start with do, then the we go to the meat and then the while which will see if we contuinue or not
+
+//Do while loops always run at least once. 
+
+//for loops funchions like while but it different
+
+//we start with for then (three sections)
+
+//first section is optional, lets us declair stuff it runs once at begingging of loop
+
+//Middle is conditional that will function every time the loop repeats
+
+//third lets us add or minus any variables and will repeat each time
+
+//semi colon to seperate these things
+
+//Lest loop uis for each which is good at going throughj lists
+
+//it loops through colections until it reaches the end of the collection. 
